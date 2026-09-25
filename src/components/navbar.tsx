57af124958c83359c7a5ba2848df4d7e5a96d62c
Tahxin <satahsin6@gmail.com>
2026-09-25 21:07:14 +0600

    added navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import Image from 'next/image';
+import Logo from '@/../public/logo.png';
 
 const Navbar = () => {
     return (
-        <div>
+        <section className="flex justify-between items-center p-4 font-oswald">
+            <div>
+                <Image src={Logo} alt="Logo" width={100} height={100} />
+                <h2>FITLOG</h2>
+            </div>
+            <div>
+                <button>Workouts</button>
+                <button>My Plan</button>
+            </div>
+            <div>
+                <button>Plan 0</button>
+                <button>Saved 0</button>
+            </div>
             
-        </div>
+        </section>
     );
 };
 
